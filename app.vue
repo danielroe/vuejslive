0bb45bfb10705e81661f164353ea940e47e904d4
Daniel Roe <daniel@roe.dev>
2023-05-12 15:42:50 +0100

    feat: add text

--- app.vue
+++ app.vue
@@ -1,10 +1,25 @@
 <script>
 import '@picocss/pico'
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      message: ref('So nice to be here!')
+    }
+  }
+}
 </script>
 
 <template>
   <div class="container">
-    Hi Vue.js London!
+    <h1>
+      👋 Hi Vue.js London!
+    </h1>
+    <p>
+      {{ message }}
+    </p>
+    <button @click="message = 'Let me know what you think!'">Click me!</button>
   </div>
 </template>
 
